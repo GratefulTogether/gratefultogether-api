217b91cbf64ab0234146e10576f4687a8a06b986
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-08-25 13:33:35 -0400

    add relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :wins, dependent: :destroy
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_171552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wins", force: :cascade do |t|
+    t.text "entry"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wins_on_user_id"
+  end
+
+  add_foreign_key "wins", "users"
 end
