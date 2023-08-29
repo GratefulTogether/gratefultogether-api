19967c868f302043fbb13d1c909fab832bc3682c
Steven Stackhouse <stackmm@gmail.com>
2023-08-29 15:18:18 -0600

    add test for successful subscription

--- spec/channels/wins_channel_spec.rb
+++ spec/channels/wins_channel_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe WinsChannel, type: :channel do
-  
+  it "successfully subscribes" do
+    subscribe
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_from("wins_channel")
+  end
 end
