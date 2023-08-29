ba152d8593a9d6c4e90b95b6d93275b7b0acd0de
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-29 13:50:09 -0600

    comment out production environment setup

--- app/channels/wins_channel.rb
+++ app/channels/wins_channel.rb
@@ -1,18 +1,8 @@
 class WinsChannel < ApplicationCable::Channel
   def subscribed
-    # if params[:date]
-    #   wins = Win.all_by_date(params[:date])
-    # else
-    #   wins = Win.all_today
-    # end
-    
     stream_from "wins_channel"
   end
-
-  # def receive(data)
-  #   ActionCable.server.broadcast(wins, data)
-  # end
-
+  
   def unsubscribed
   end
 end
