308c1baa2252517c95541e94d9b1536763101be4
K.D. Hubbard <hubbard.kd@gmail.com>
2023-08-28 21:36:02 -0600

    wip websocket connection

--- app/channels/wins_channel.rb
+++ app/channels/wins_channel.rb
@@ -1,19 +1,18 @@
 class WinsChannel < ApplicationCable::Channel
   def subscribed
-    if params[:date]
-      wins = Win.all_by_date(params[:date])
-    else
-      wins = Win.all_today
-    end
+    # if params[:date]
+    #   wins = Win.all_by_date(params[:date])
+    # else
+    #   wins = Win.all_today
+    # end
+    puts "blah di blah"
     require 'pry'; binding.pry
     stream_for wins
   end
 
-  # def receive(data)
-    
-  #   # ActionCable.server.broadcast("chat_#{params[:room]}", data)
-  #   # puts data
-  # end
+  def receive(data)
+    ActionCable.server.broadcast(wins, data)
+  end
 
   def unsubscribed
   end
