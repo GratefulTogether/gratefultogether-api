83fcc8edc9316f165551cce4137b3254c651f7a7
Jesse Sorman <jessesorman@gmail.com>
2023-09-01 14:04:32 -0400

    update cors and action cable config to only accept FE

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://gratefultogether.vercel.app/"
+    origins "https://gratefultogether-68rtro4pr-caliham.vercel.app/"
 
     resource "*",
       headers: :any,
